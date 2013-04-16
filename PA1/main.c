ff31d951a567e37853ef72c93a10ab40b39663d1
Shawn Adams <sma92176@gmail.com>
2013-04-16 14:17:34 -0700

    Initial Commit

---
+++ PA1/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  PA1
+//
+//  Created by Shawn Adams on 4/16/13.
+//  Copyright (c) 2013 Shawn Adams. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
